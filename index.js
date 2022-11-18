38729174f1b353505610ab6584fcce6feca387a6
ayeasin14 <ayeasin14@gmail.com>
2022-11-18 16:32:41 +0600

    added new

--- index.js
+++ index.js
@@ -178,6 +178,14 @@ async function run() {
         });
 
 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+
 
         app.post('/users', async (req, res) => {
             const user = req.body;
